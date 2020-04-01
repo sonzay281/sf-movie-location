ccfee26c18f2a40c3f67526274278fc8ed60026e
Sanjaya Sapkota <sonzayspk1998@gmail.com>
2020-04-01 17:27:05 +0545

    master: add downshift autocomplete component

---
+++ src/components/autocomplete/AutoComplete.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useRef } from "react";
+import Downshift from "downshift";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+interface AutoComplete {
+  id: string;
+  placeholder?: string;
+  listIcon?: any;
+  items: any;
+  onChange: any;
+  onClear: any;
+  value: string;
+}
+const AutoComplete = ({
+  id,
+  placeholder,
+  listIcon,
+  items,
+  onChange,
+  value,
+  onClear
+}: AutoComplete) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref: any = useRef(null);
+  const onClick = () => {
+    setIsOpen(false);
+    ref.current.blur();
+  };
+
+  return (
+    <Downshift
+      onChange={onChange}
+      itemToString={(item: any) => item || ""}
+      initialInputValue={value}
+      inputId={id}
+    >
+      {({ getInputProps, inputValue, getItemProps }: any) => (
+        <div className={`${id}-wrapper`}>
+          <div className="widget-header-content">
+            <input
+              className={id}
+              id={id}
+              placeholder={placeholder}
+              ref={ref}
+              {...getInputProps()}
+              onFocus={() => setIsOpen(true)}
+              onBlur={() => setIsOpen(false)}
+              value={value}
+            />
+            <div className="search-icon">
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <div className="clear">
+              <FontAwesomeIcon icon={faTimes} onClick={onClear} />
+            </div>
+          </div>
+
+          {isOpen && (
+            <div className="widget--opened scroll" onClick={onClick}>
+              {!!inputValue
+                ? items
+                    ?.filter((i: any) =>
+                      i?.toUpperCase().includes(inputValue?.toUpperCase())
+                    )
+                    .map((item: string, index: number) => (
+                      <div
+                        className="movie__item padding-content"
+                        {...getItemProps({
+                          key: item,
+                          index,
+                          item
+                        })}
+                      >
+                        {listIcon && (
+                          <span className="icon margin-r--small">
+                            <FontAwesomeIcon icon={listIcon} />
+                            &nbsp;&nbsp;
+                          </span>
+                        )}
+                        {item}
+                      </div>
+                    ))
+                : ""}
+            </div>
+          )}
+        </div>
+      )}
+    </Downshift>
+  );
+};
+
+export default AutoComplete;
