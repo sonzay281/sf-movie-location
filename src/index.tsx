fb2edb212980f964b542f3c5e231a8e8a1a5a747
Sanjaya Sapkota <sonzayspk1998@gmail.com>
2020-04-05 20:32:58 +0545

    master: add error logging component and integrate with sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { init } from "@sentry/browser";
 
 import store from "store";
 import * as serviceWorker from "./serviceWorker";
 
 import "assets/scss/styles.scss";
-import LandingPage from "views";
+import ErrorBoundary from "views/ErrorBoundary";
+import LandingPage from "views/LandingPage";
 
+init({
+  dsn: "https://a1453a9088b54430b1e720c5d247b6c9@sentry.io/5189353",
+});
 ReactDOM.render(
   <Provider store={store}>
-    <LandingPage />
+    <ErrorBoundary>
+      <LandingPage />
+    </ErrorBoundary>
   </Provider>,
   document.getElementById("root")
 );
