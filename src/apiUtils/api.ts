3a9ce3fb03b2dd8ae20b702c67fe34f6e2f7b3f3
Sanjaya Sapkota <sonzayspk1998@gmail.com>
2020-04-05 12:21:12 +0545

    master: remove unnecessary code blocks

--- src/apiUtils/api.ts
+++ src/apiUtils/api.ts
@@ -3,42 +3,14 @@ import axios from "axios";
 export const http = axios.create({
   baseURL: process.env.REACT_APP_DATA_BASE_URL,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-// http.interceptors.response.use(
-//   response => {
-//     if (response?.data?.access_token) {
-//       window.localStorage.setItem("token", response.data.access_token);
-//     }
-//     return response;
-//   },
-//   error => {
-//     const { response } = error;
-//     if (response?.status === 401) {
-//       window.localStorage.removeItem("token");
-//       window.location.pathname = "/";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-http.interceptors.request.use(config => {
+http.interceptors.request.use((config) => {
   if (!!process.env.REACT_APP_DATA_APP_TOKEN)
     config.headers["X-App-Token"] = process.env.REACT_APP_DATA_APP_TOKEN;
   return config;
 });
 
-export const del = (url: string) => http.delete(url);
-
 export const get = ({ url }: { url: string }) => http.get(url);
-
-export const put = ({ url, data }: { url: string; data: any }) =>
-  http.put(url, data);
-
-export const post = ({ url, data }: { url: string; data: any }) =>
-  http.post(url, data);
-
-export const patch = ({ url, data }: { url: string; data: any }) =>
-  http.patch(url, data);
--- src/sagas/genericSagas.tsx
+++ src/sagas/genericSagas.tsx
@@ -1,44 +1,11 @@
 import { put, call } from "redux-saga/effects";
 
-import { get, post, del, patch } from "apiUtils/api";
+import { get } from "apiUtils/api";
 
 export function* fetchData({ payload }: any) {
   try {
-    const {data} = yield call(get, payload);
-    yield put({ type: payload.onSuccess, response:data });
-  } catch (error) {
-    yield put({ type: payload.onFailure, error });
-  }
-}
-
-export function* postData({ payload }: any) {
-  try {
-    const {
-      data: { response }
-    } = yield call(post, payload);
-    yield put({ type: payload.onSuccess, response });
-  } catch (error) {
-    yield put({ type: payload.onFailure, error });
-  }
-}
-
-export function* deleteData({ payload }: any) {
-  try {
-    const {
-      data: { response }
-    } = yield call(del, payload);
-    yield put({ type: payload.onSuccess, response });
-  } catch (error) {
-    yield put({ type: payload.onFailure, error });
-  }
-}
-
-export function* patchData({ payload }: any) {
-  try {
-    const {
-      data: { response }
-    } = yield call(patch, payload);
-    yield put({ type: payload.onSuccess, response });
+    const { data } = yield call(get, payload);
+    yield put({ type: payload.onSuccess, response: data });
   } catch (error) {
     yield put({ type: payload.onFailure, error });
   }
